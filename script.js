12f060aceb8bc2a703f60a7afff0118eead9413b
Alex Pelavin <ajpele@gmail.com>
2016-11-03 19:02:14 -0700

    Projects page with git, bitbucket, and placeholder

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 var app = angular.module('App', ['ngRoute']);
-app.controller('NavCtrl', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
-
-    $scope.links = {
-        'home':       { location: '/',           glyph: 'glyphicon-home',      text: 'Home',       },
-        'projects':   { location: '/projects',   glyph: 'glyphicon-console',   text: 'Projects',   },
-        'experience': { location: '/experience', glyph: 'glyphicon-paperclip', text: 'Experience', },
-        'about':      { location: '/about',      glyph: 'glyphicon-user',      text: 'About Me',   },
-        'contact':    { location: '/contact',    glyph: 'glyphicon-envelope',  text: 'Contact Me', }
-    };
-});
 
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -32,5 +19,37 @@ app.config(function($routeProvider, $locationProvider) {
         })
         .otherwise( {
             templateUrl: 'views/error.html'
+        });
+});
+
+app.controller('NavCtrl', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+
+    $scope.links = {
+        'home':       { location: '/',           glyph: 'glyphicon-home',      text: 'Home',       },
+        'projects':   { location: '/projects',   glyph: 'glyphicon-console',   text: 'Projects',   },
+        'experience': { location: '/experience', glyph: 'glyphicon-paperclip', text: 'Experience', },
+        'about':      { location: '/about',      glyph: 'glyphicon-user',      text: 'About Me',   },
+        'contact':    { location: '/contact',    glyph: 'glyphicon-envelope',  text: 'Contact Me', }
+    };
+});
+
+app.controller('ProjectsCtrl', function($scope, $http) {
+    var github_user = 'Lazzer64';
+    var bbucket_user = 'a_pelavin';
+
+    $scope.github_open = false;
+    $scope.bbucket_open = false;
+
+    $http.get('https://api.github.com/users/'+github_user+'/repos')
+        .then(function(resp) {
+            var repos = resp['data'];
+            $scope.github_repos = repos;
+        }); $http.get('https://api.bitbucket.org/2.0/repositories/'+bbucket_user)
+        .then(function(resp) {
+            var repos = resp['data']['values'];
+            $scope.bbucket_repos = repos;
         });
 });
