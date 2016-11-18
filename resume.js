f51bc6526fbbe503a8bae10f34e30a063cc32060
Alex Pelavin <ajpele@gmail.com>
2016-11-17 16:19:30 -0800

    Updated texts

--- resume.js
+++ resume.js
@@ -1,19 +1,22 @@
 var resume = [
     {
         subject: 'Education',
-        heading: 'h2',
         items: [
             'University of the Pacific (Class of 2019) working towards B.S. in Computer Science',
             'Dean\'s List Fall 2015 - Fall 2016',
             'Graduated from Palo Alto High School (Class of 2015)',
-            'Relevant Coursework', 
-            'Advanced Placement Computer Science',
-            'Advanced Programing Methodologies in C++',
-            'Application Development',
-            'Computer Systems and Networks',
-            'Data Structures',
-            'Digital Design',
-            'Web Applications',
+            {
+                subject: 'Relevant Coursework',
+                items: [
+                    'Advanced Placement Computer Science',
+                    'Advanced Programing Methodologies in C++',
+                    'Application Development',
+                    'Computer Systems and Networks',
+                    'Data Structures',
+                    'Digital Design',
+                    'Web Applications',
+                ],
+            }
         ]
     },
     {
@@ -36,14 +39,16 @@ var resume = [
         subject: 'Experience',
         items: [
             {
-                subject: 'Interned at startup Reactor8 Summer of 2015',
+                subject: 'Interned at startup Reactor8',
+                date: 'Summer of 2015',
                 items: [
                     'Worked on software framework that automates deployment of  big data stacks',
                     'Focused on automated deployment of Apache Spark clusters and automated builds of Scala apps',
                 ],
             },
             {
-                subject: 'Information Technology Assistant for the PAUSD school district Summer of 2014',
+                subject: 'Information Technology Assistant for the PAUSD school district',
+                date: 'Summer of 2014',
                 items: [
                     'Troubleshot and solved software and hardware issues.',
                     'Created and deployed system images to large number of computers.',
